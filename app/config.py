d6a29b0994fd2b131286f70219511dd9569ae14c
Martins Ayoola <omartins365@gmail.com>
2025-06-16 07:16:24 +0100

    Update devcontainer and config for Gradio integration: rename to 'Cashew YOLOv10 Gradio', adjust model paths.

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 MODEL_URL = "https://github.com/THU-MIG/yolov10/releases/download/v1.1/yolov10n.pt"
 DEMO_FILES_URL = "https://drive.google.com/drive/folders/15mKocsFZ5L9EceynG5_x-Y6KvkE85pS0?usp=sharing"
-MODEL_PATH = os.path.join(os.getcwd(), "app", "model", "yolov10n.pt")
+MODEL_PATH = os.path.join(os.getcwd(), "app", "model", "yolov10s.pt")
 RESULT_PATH = os.path.join(os.getcwd(), "app", "result")
 
 IMAGE_WIDTH = 550
@@ -13,4 +13,4 @@ EPOCHS = 100
 IMG_SIZE = 640
 BATCH_SIZE = 24
 
-TRAINED_MODEL_PATH = "C:\Users\Martins\WorkSpace\cashew_yolo\cashew_model\weights\best.pt"# os.path.join(os.getcwd(), "app", "yolov10", "runs", "detect", "train", "weights", "best.pt")
+TRAINED_MODEL_PATH = os.path.join(os.getcwd(), "app", "model", "best.pt")
